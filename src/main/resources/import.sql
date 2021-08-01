85030d3fe8e4f5776e7483447c5aa0cde3de78aa
gabrielschulz <gabpaesschulzzz@gmail.com>
2021-08-01 14:09:44 -0300

    exercicio finalizado

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Gabriel Schimidt', '02547856325', 6500.0, TIMESTAMP WITH TIME ZONE '1994-02-15T10:30:00Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Rodrigo Porto', '60237460041', 3200.0, TIMESTAMP WITH TIME ZONE '1989-02-15T10:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Vanessa Barbosa', '56770073072', 7200.0, TIMESTAMP WITH TIME ZONE '1998-10-25T03:00:00Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ivan Assis ', '74373050031', 1200.0, TIMESTAMP WITH TIME ZONE '1991-02-21T10:04:00Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Eveline Carreiro', '66965309083', 2000.0, TIMESTAMP WITH TIME ZONE '1990-01-30T05:00:00Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Isabel Pacheco', '99820634059', 5800.0, TIMESTAMP WITH TIME ZONE '1992-02-05T06:00:00Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Alissa Anderson', '62530994005', 9600.0, TIMESTAMP WITH TIME ZONE '1993-08-11T08:30:00Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Nicoli Cerqueira', '99183109013', 3100.0, TIMESTAMP WITH TIME ZONE '1996-06-09T12:00:00Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Hamilton Bezerra', '29674556010', 4200.0, TIMESTAMP WITH TIME ZONE '1997-04-17T09:00:00Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Vincente Mafra', '98651940076', 8900.0, TIMESTAMP WITH TIME ZONE '1995-02-09T11:35:00Z', 4);
